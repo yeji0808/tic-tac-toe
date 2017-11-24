1f0a331de145e0c392591e5c306f2d8eb9564575
Yeji Lee <yeji080808@gmail.com>
2017-11-24 05:50:05 -0800

    add win condition

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,13 +1,21 @@
 from tkinter import *
+import tkinter.messagebox
 
 def checked(i) :
       global player
       button = list[i]
+      win = ((0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6))
+  
 
       if button["text"] != "     " :
             return
       button["text"] = player 
       button["bg"] = "yellow"
+
+      for a in win:
+            if player == list[a[0]]["text"] == list[a[1]]["text"] == list[a[2]]["text"]:
+                   tkinter.messagebox.showinfo("게임종료", "{0}가 이겼습니다!".format(player))
+                   quit()
 
       if player == "X" :
             player = "O"
@@ -15,6 +23,10 @@ def checked(i) :
       else :
             player = "X"
             button["bg"] = "lightgreen"
+
+def quit():
+      for b in list:
+            b["command"] = ""
 
 window = Tk()
 player = "X"
